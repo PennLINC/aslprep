9e1577980c86e122818d706d5b4512eb44a7674e
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2020-05-28 21:58:33 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ from github_link import make_linkcode_resolve
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "1.5.3"
+#needs_sphinx = "2.4.4"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
