e0a4972f2faa50ceb25595c2cba810b2e16b4fa8
Taylor Salo <tsalo006@fiu.edu>
2023-02-08 14:54:23 -0500

    Fix.

--- aslprep/tests/test_cli.py
+++ aslprep/tests/test_cli.py
@@ -7,12 +7,12 @@ from aslprep.cli.run import build_workflow, get_parser
 from aslprep.tests.utils import check_affines, check_generated_files, get_test_data_path
 
 
-@pytest.mark.dset
-def test_dset(datasets, output_dir, working_dir):
-    """Run xcp_d on ds001419 fMRIPrep derivatives, with nifti options."""
-    test_name = "test_dset"
+@pytest.mark.sub01
+def test_sub01(datasets, output_dir, working_dir):
+    """Run aslprep on sub-01 data."""
+    test_name = "test_sub01"
 
-    data_dir = datasets["dset"]
+    data_dir = datasets["sub01"]
     smriprep_dir = datasets["smriprep"]
     out_dir = os.path.join(output_dir, test_name)
     work_dir = os.path.join(working_dir, test_name)
@@ -24,6 +24,7 @@ def test_dset(datasets, output_dir, working_dir):
         data_dir,
         out_dir,
         "participant",
+        "--participant-label=01",
         f"-w={work_dir}",
         "--nthreads=2",
         "--omp-nthreads=2",
@@ -42,7 +43,49 @@ def test_dset(datasets, output_dir, working_dir):
     plugin_settings = retval["plugin_settings"]
     aslprep_wf.run(**plugin_settings)
 
-    output_list_file = os.path.join(test_data_dir, "aslprep_outputs.txt")
+    output_list_file = os.path.join(test_data_dir, "test_outputs_sub01.txt")
+    check_generated_files(out_dir, output_list_file)
+
+    check_affines(data_dir, out_dir, input_type="nifti")
+
+
+@pytest.mark.subA00086748
+def test_subA00086748(datasets, output_dir, working_dir):
+    """Run aslprep on sub-A00086748."""
+    test_name = "test_subA00086748"
+
+    data_dir = datasets["dset"]
+    smriprep_dir = datasets["smriprep"]
+    out_dir = os.path.join(output_dir, test_name)
+    work_dir = os.path.join(working_dir, test_name)
+
+    test_data_dir = get_test_data_path()
+    os.environ["FS_LICENSE"] = os.path.join(test_data_dir, "license.txt")
+
+    parameters = [
+        data_dir,
+        out_dir,
+        "participant",
+        "--participant-label=A00086748",
+        f"-w={work_dir}",
+        "--nthreads=2",
+        "--omp-nthreads=2",
+        "--output-spaces=asl",
+        "--scorescrub",
+        "--basil",
+        "--use-syn-sdc",
+        f"--anat-derivatives={smriprep_dir}",
+    ]
+    opts = get_parser().parse_args(parameters)
+
+    retval = {}
+    retval = build_workflow(opts, retval=retval)
+    # run_uuid = retval.get("run_uuid", None)
+    aslprep_wf = retval.get("workflow", None)
+    plugin_settings = retval["plugin_settings"]
+    aslprep_wf.run(**plugin_settings)
+
+    output_list_file = os.path.join(test_data_dir, "test_outputs_subA00086748.txt")
     check_generated_files(out_dir, output_list_file)
 
     check_affines(data_dir, out_dir, input_type="nifti")
