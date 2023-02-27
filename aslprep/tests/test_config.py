40c4217bf4c2a207a6e9581dae62e00725c79493
Taylor Salo <tsalo006@fiu.edu>
2023-02-27 15:00:56 -0500

    Remove AROMA test.

--- aslprep/tests/test_config.py
+++ aslprep/tests/test_config.py
@@ -29,20 +29,7 @@ def test_config_spaces():
         if s.standard and s.dim == 3
     ]
 
-    config.workflow.use_aroma = True
-    config.init_spaces()
-    spaces = config.workflow.spaces
-
-    assert "MNI152NLin6Asym:res-2" in [str(s) for s in spaces.get_standard(full_spec=True)]
-
-    assert "MNI152NLin6Asym_res-2" in [
-        format_reference((s.fullname, s.spec))
-        for s in spaces.references
-        if s.standard and s.dim == 3
-    ]
-
     config.execution.output_spaces = None
-    config.workflow.use_aroma = False
     config.init_spaces()
     spaces = config.workflow.spaces
 
