229f3d5d6823e1a85cc138c68c74fff292dff676
Taylor Salo <tsalo006@fiu.edu>
2023-03-28 11:43:26 -0400

    Update cbf_computation.py

--- aslprep/interfaces/cbf_computation.py
+++ aslprep/interfaces/cbf_computation.py
@@ -395,6 +395,7 @@ class ComputeCBF(SimpleInterface):
             # Wang et al. (2013): https://doi.org/10.1016%2Fj.nicl.2013.06.017
             # Dai et al. (2012): https://doi.org/10.1002/mrm.23103
             cbf_ts = deltam_scaled * perfusion_factor[None, :]
+            cbf = np.zeros((n_voxels, np.unique(perfusion_factor).size))
             for perfusion_value in np.unique(perfusion_factor):
                 perf_val_idx = perfusion_factor == perfusion_value
                 cbf[:, perf_val_idx] = np.sum(cbf_ts[:, perf_val_idx], axis=1) / np.sum(
