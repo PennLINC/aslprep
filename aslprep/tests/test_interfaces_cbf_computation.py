fbaf13a234874532ac3d402f34b175270ba3826a
Taylor Salo <tsalo006@fiu.edu>
2023-05-18 10:30:52 -0400

    Update test_interfaces_cbf_computation.py

--- aslprep/tests/test_interfaces_cbf_computation.py
+++ aslprep/tests/test_interfaces_cbf_computation.py
@@ -89,10 +89,7 @@ def test_computecbf_casl(datasets, tmp_path_factory):
                 m0_file=m0_file,
                 mask=mask_file,
             )
-            with pytest.raises(
-                ValueError,
-                match="ASLPrep cannot currently process multi-PLD data.",
-            ):
+            with pytest.raises(ValueError, match="Number of PostLabelingDelays"):
                 results = interface.run(cwd=tmpdir)
 
             # Scenario 3: PCASL with one PostLabelingDelay for each deltam volume (good)
@@ -111,11 +108,14 @@ def test_computecbf_casl(datasets, tmp_path_factory):
                 m0_file=m0_file,
                 mask=mask_file,
             )
-            with pytest.raises(
-                ValueError,
-                match="ASLPrep cannot currently process multi-PLD data.",
-            ):
-                results = interface.run(cwd=tmpdir)
+            results = interface.run(cwd=tmpdir)
+            assert os.path.isfile(results.outputs.cbf_ts)
+            cbf_img = nb.load(results.outputs.cbf_ts)
+            assert cbf_img.ndim == 4
+            assert cbf_img.shape[3] == n_deltam
+            assert os.path.isfile(results.outputs.mean_cbf)
+            mean_cbf_img = nb.load(results.outputs.mean_cbf)
+            assert mean_cbf_img.ndim == 3
 
 
 def test_computecbf_pasl(datasets, tmp_path_factory):
