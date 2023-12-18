1f07271dd5212f3d9c65a8fb480e78331a40aa17
Taylor Salo <tsalo006@fiu.edu>
2023-12-18 09:41:41 -0500

    Run isort.

--- aslprep/utils/__init__.py
+++ aslprep/utils/__init__.py
@@ -1,14 +1,5 @@
 """Utility functions for aslprep."""
-from aslprep.utils import (
-    asl,
-    atlas,
-    bids,
-    cbf,
-    confounds,
-    misc,
-    plotting,
-    sentry,
-)
+from aslprep.utils import asl, atlas, bids, cbf, confounds, misc, plotting, sentry
 
 __all__ = [
     "asl",
