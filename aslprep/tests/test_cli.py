02b999d47a3a8dfb2dddbccd0d169981496f3b0a
Taylor Salo <tsalo006@fiu.edu>
2023-12-22 15:50:30 -0500

    Update tests.

--- aslprep/tests/test_cli.py
+++ aslprep/tests/test_cli.py
@@ -54,8 +54,7 @@ def test_examples_pasl_multipld(data_dir, output_dir, working_dir):
         f"{os.path.join(data_dir, 'anatomical/smriprep')}",
     ]
 
-    # _run_and_generate(TEST_NAME, PARTICIPANT_LABEL, parameters, out_dir)
-    _run_and_fail(parameters)
+    _run_and_generate(TEST_NAME, PARTICIPANT_LABEL, parameters, out_dir)
 
 
 @pytest.mark.examples_pcasl_multipld
