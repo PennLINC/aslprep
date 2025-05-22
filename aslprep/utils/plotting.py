d46ec61dca053d973878f39ae9128708511b14c8
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-05-22 10:40:20 -0400

    chore: Use nireports fMRIPlot class, vendored svgutils

--- aslprep/utils/plotting.py
+++ aslprep/utils/plotting.py
@@ -1,22 +1,17 @@
 """Plotting functions and classes."""
 
-import matplotlib.pyplot as plt
 import nibabel as nb
-import numpy as np
-import pandas as pd
 from lxml import etree
-from matplotlib import gridspec as mgs
 from nilearn import image, plotting
 from nilearn._utils.niimg import load_niimg
-from niworkflows import NIWORKFLOWS_LOG
-from niworkflows.viz.plots import confoundplot, spikesplot
-from niworkflows.viz.utils import (
+from nireports._vendored.svgutils.transform import SVGFigure
+from nireports.reportlets.utils import (
     compose_view,
     cuts_from_bbox,
     extract_svg,
     robust_set_limits,
 )
-from svgutils.transform import SVGFigure
+from niworkflows import NIWORKFLOWS_LOG
 
 
 class CBFPlot:
@@ -111,114 +106,3 @@ def plot_stat_map(
         out_files.append(svg_fig)
 
     return out_files
-
-
-class fMRIPlot:  # noqa:N801
-    """Generates the fMRI Summary Plot."""
-
-    __slots__ = (
-        'timeseries',
-        'segments',
-        'tr',
-        'confounds',
-        'spikes',
-        'nskip',
-        'sort_carpet',
-        'paired_carpet',
-    )
-
-    def __init__(
-        self,
-        timeseries,
-        segments,
-        confounds=None,
-        conf_file=None,
-        tr=None,
-        usecols=None,
-        units=None,
-        vlines=None,
-        spikes_files=None,
-        nskip=0,
-        sort_carpet=True,
-        paired_carpet=False,
-    ):
-        self.timeseries = timeseries
-        self.segments = segments
-        self.tr = tr
-        self.nskip = nskip
-        self.sort_carpet = sort_carpet
-        self.paired_carpet = paired_carpet
-
-        if units is None:
-            units = {}
-        if vlines is None:
-            vlines = {}
-        self.confounds = {}
-        if confounds is None and conf_file:
-            confounds = pd.read_csv(conf_file, sep=r'[\t\s]+', usecols=usecols, index_col=False)
-
-        if confounds is not None:
-            for name in confounds.columns:
-                self.confounds[name] = {
-                    'values': confounds[[name]].values.squeeze().tolist(),
-                    'units': units.get(name),
-                    'cutoff': vlines.get(name),
-                }
-
-        self.spikes = []
-        if spikes_files:
-            for sp_file in spikes_files:
-                self.spikes.append((np.loadtxt(sp_file), None, False))
-
-    def plot(self, figure=None):
-        """Generate fMRI plot."""
-        import seaborn as sns
-        from niworkflows.viz.plots import plot_carpet as plt_carpet
-
-        sns.set_style('whitegrid')
-        sns.set_context('paper', font_scale=0.8)
-
-        if figure is None:
-            figure = plt.gcf()
-
-        nconfounds = len(self.confounds)
-        nspikes = len(self.spikes)
-        nrows = 1 + nconfounds + nspikes
-
-        # Create grid
-        grid = mgs.GridSpec(
-            nrows, 1, wspace=0.0, hspace=0.05, height_ratios=[1] * (nrows - 1) + [5]
-        )
-
-        grid_id = 0
-        for tsz, name, iszs in self.spikes:
-            spikesplot(tsz, title=name, outer_gs=grid[grid_id], tr=self.tr, zscored=iszs)
-            grid_id += 1
-
-        if self.confounds:
-            from seaborn import color_palette
-
-            palette = color_palette('husl', nconfounds)
-
-        for i, (name, kwargs) in enumerate(self.confounds.items()):
-            tseries = kwargs.pop('values')
-            try:
-                confoundplot(
-                    tseries, grid[grid_id], tr=self.tr, color=palette[i], name=name, **kwargs
-                )
-            except Exception:  # noqa: BLE001
-                raise ValueError(name) from None
-            grid_id += 1
-
-        plt_carpet(
-            self.timeseries,
-            segments=self.segments,
-            subplot=grid[-1],
-            tr=self.tr,
-            sort_rows=self.sort_carpet,
-            drop_trs=self.nskip,
-            cmap='paired' if self.paired_carpet else None,
-            # This is the only modification we need for ASLPrep
-            detrend=False,
-        )
-        return figure
