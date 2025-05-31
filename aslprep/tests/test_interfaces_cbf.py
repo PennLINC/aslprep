a5b19e920eaf5dff3ac05ab994b33ccbc0429e13
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-05-31 10:19:16 -0400

    Update test_interfaces_cbf.py

--- aslprep/tests/test_interfaces_cbf.py
+++ aslprep/tests/test_interfaces_cbf.py
@@ -223,7 +223,7 @@ def test_computecbf_pasl(datasets, tmp_path_factory):
             m0_file=m0_file,
             mask=mask_file,
         )
-        with pytest.raises(ValueError, match='Multi-delay data are not supported'):
+        with pytest.raises(ValueError, match='Unsupported BolusCutOffTechnique (QUIPSS)'):
             results = interface.run(cwd=tmpdir)
 
         # Scenario 4: QUIPSS PASL with one PostLabelingDelay for each deltam volume (good)
@@ -243,8 +243,10 @@ def test_computecbf_pasl(datasets, tmp_path_factory):
             m0_file=m0_file,
             mask=mask_file,
         )
-        with pytest.raises(ValueError, match='Multi-delay data are not supported'):
-            results = interface.run(cwd=tmpdir)
+        results = interface.run(cwd=tmpdir)
+        assert os.path.isfile(results.outputs.mean_cbf)
+        mean_cbf_img = nb.load(results.outputs.mean_cbf)
+        assert mean_cbf_img.ndim == 3
 
         # Scenario 5: QUIPSSII PASL with one PostLabelingDelay
         # This should produce CBF time series and mean CBF, but no ATT
@@ -290,8 +292,10 @@ def test_computecbf_pasl(datasets, tmp_path_factory):
             m0_file=m0_file,
             mask=mask_file,
         )
-        with pytest.raises(ValueError, match='Multi-delay data are not supported'):
-            results = interface.run(cwd=tmpdir)
+        results = interface.run(cwd=tmpdir)
+        assert os.path.isfile(results.outputs.mean_cbf)
+        mean_cbf_img = nb.load(results.outputs.mean_cbf)
+        assert mean_cbf_img.ndim == 3
 
         # Scenario 7: Q2TIPS PASL with one PostLabelingDelay
         # This should produce CBF time series and mean CBF, but no ATT
@@ -337,8 +341,10 @@ def test_computecbf_pasl(datasets, tmp_path_factory):
             m0_file=m0_file,
             mask=mask_file,
         )
-        with pytest.raises(ValueError, match='Multi-delay data are not supported'):
-            results = interface.run(cwd=tmpdir)
+        results = interface.run(cwd=tmpdir)
+        assert os.path.isfile(results.outputs.mean_cbf)
+        mean_cbf_img = nb.load(results.outputs.mean_cbf)
+        assert mean_cbf_img.ndim == 3
 
 
 def test_compare_slicetiming(datasets, tmp_path_factory):
